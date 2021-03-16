6c11eb8434832253f96931250b97a1839a8e0d70
Álvaro Cuesta <alvaro-cuesta@GitHub>
2021-03-16 12:52:37 +0100

    Migrate PatternTable to hooks

--- src/components/PatternTable.jsx
+++ src/components/PatternTable.jsx
@@ -1,7 +1,6 @@
 import { smoothScroll, easeOutSine } from '../smoothScroll'
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useCallback, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import ClapRow from './ClapRow'
@@ -10,122 +9,124 @@ const EMPTY_ROWS = 1
 const SCROLL_DURATION = 300
 const SCROLL_EASING = easeOutSine
 
-class PatternTable extends React.PureComponent {
-  constructor(props) {
-    super(props)
+const PatternTable = ({
+  buttonHandler,
+  buttonLabel,
+  clapPattern,
+  pattern,
+  pulse,
+  repeats,
+  userInput,
+  className,
+}) => {
+  let originalPattern = pattern
 
-    this.rows = []
-  }
+  if (pattern === undefined || pattern <= 0) pattern = 0
 
-  componentDidMount() {
-    this.scrollAnchor.scrollTop =
-      this.rows[0] - this.scrollAnchor.offsetTop + this.rows[0].offsetHeight - 4
-  }
+  const rowsRef = useRef([])
+  const scrollAnchorRef = useRef(null)
+  const isFirstRenderRef = useRef(true)
 
-  componentWillReceiveProps({ pattern }) {
-    if (this.props.pattern !== pattern) {
-      if (pattern === undefined || pattern <= 0) pattern = 0
-
-      this.scrollToPattern(pattern)
-    }
-  }
-
-  scrollToPattern(i) {
+  const scrollToPattern = useCallback((i) => {
     smoothScroll(
       document.scrollingElement,
-      this.rows[i].offsetTop -
-        this.scrollAnchor.offsetTop +
-        this.rows[i].offsetHeight -
+      rowsRef.current[i].offsetTop -
+        scrollAnchorRef.current.offsetTop +
+        rowsRef.current[i].offsetHeight -
         /* HACK */ 4,
       SCROLL_DURATION,
       SCROLL_EASING,
     )
-  }
+  }, [])
 
-  render() {
-    let {
-      buttonHandler,
-      buttonLabel,
-      clapPattern,
-      pattern,
-      pulse,
-      repeats,
-      userInput,
-      className,
-    } = this.props
+  // TODO: What does this do?
+  useEffect(() => {
+    scrollAnchorRef.current.scrollTop =
+      rowsRef.current[0] -
+      scrollAnchorRef.current.offsetTop +
+      rowsRef.current[0].offsetHeight -
+      4
+  }, [])
 
-    let originalPattern = pattern
+  useEffect(() => {
+    if (isFirstRenderRef.current) {
+      isFirstRenderRef.current = false
+    } else {
+      scrollToPattern(pattern === undefined || pattern <= 0 ? 0 : pattern)
+    }
+  }, [pattern, scrollToPattern])
 
-    if (pattern === undefined || pattern <= 0) pattern = 0
-
-    return (
-      <div
-        className={`pattern-table noselect ${className}`}
-        ref={(e) => (this.scrollAnchor = ReactDOM.findDOMNode(e))}
-      >
-        <div className="arrow-row">
-          <div className="head">
-            <button className="noselect" onClick={buttonHandler}>
-              {buttonLabel}
-            </button>
-          </div>
-          <div className="pattern">
-            {clapPattern.map((_, i) => (
-              <div key={i} className="cell arrow">
-                {originalPattern >= -1 && i === pulse ? '▼' : ''}
-              </div>
-            ))}
-          </div>
+  return (
+    <div
+      className={`pattern-table noselect ${className}`}
+      ref={(e) => {
+        scrollAnchorRef.current = e
+      }}
+    >
+      <div className="arrow-row">
+        <div className="head">
+          <button className="noselect" onClick={buttonHandler}>
+            {buttonLabel}
+          </button>
         </div>
-
-        <div className="pattern-clap1">
-          <ClapRow
-            clapPattern={clapPattern}
-            head={'Clap 1 ➡'}
-            shift={0}
-            highlightPulse={pulse}
-          />
-        </div>
-
-        <div className="pattern-clap2">
-          {Array.apply(null, { length: EMPTY_ROWS }).map((_, i) => (
-            <ClapRow
-              key={i}
-              ref={(e) => {
-                this.rows[i] = e
-              }}
-              clapPattern={clapPattern}
-              className="empty"
-            />
-          ))}
-          {Array.apply(null, {
-            length: (clapPattern.length + 1) * repeats,
-          }).map((_, i) => (
-            <ClapRow
-              key={i}
-              ref={(e) => (this.rows[i + EMPTY_ROWS] = ReactDOM.findDOMNode(e))}
-              clapPattern={clapPattern}
-              className={i === pattern ? 'highlight' : ''}
-              highlightPulse={i === pattern ? pulse : undefined}
-              head={i === pattern ? 'Clap 2 ➡' : ''}
-              shift={Math.floor(i / repeats)}
-              userInput={userInput[i]}
-              donePattern={i < originalPattern}
-              currentPattern={i === originalPattern}
-            />
-          ))}
-          <div className="padder">
-            <div className="head" />
-            <div className="pattern empty">
-              {clapPattern.map((_, i) => {
-                return <div key={i} className="cell pulse" />
-              })}
+        <div className="pattern">
+          {clapPattern.map((_, i) => (
+            <div key={i} className="cell arrow">
+              {originalPattern >= -1 && i === pulse ? '▼' : ''}
             </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="pattern-clap1">
+        <ClapRow
+          clapPattern={clapPattern}
+          head={'Clap 1 ➡'}
+          shift={0}
+          highlightPulse={pulse}
+        />
+      </div>
+
+      <div className="pattern-clap2">
+        {Array.apply(null, { length: EMPTY_ROWS }).map((_, i) => (
+          <ClapRow
+            key={i}
+            ref={(e) => {
+              rowsRef.current[i] = e
+            }}
+            clapPattern={clapPattern}
+            className="empty"
+          />
+        ))}
+        {Array.apply(null, {
+          length: (clapPattern.length + 1) * repeats,
+        }).map((_, i) => (
+          <ClapRow
+            key={i}
+            ref={(e) => {
+              rowsRef.current[i + EMPTY_ROWS] = e
+            }}
+            clapPattern={clapPattern}
+            className={i === pattern ? 'highlight' : ''}
+            highlightPulse={i === pattern ? pulse : undefined}
+            head={i === pattern ? 'Clap 2 ➡' : ''}
+            shift={Math.floor(i / repeats)}
+            userInput={userInput[i]}
+            donePattern={i < originalPattern}
+            currentPattern={i === originalPattern}
+          />
+        ))}
+        <div className="padder">
+          <div className="head" />
+          <div className="pattern empty">
+            {clapPattern.map((_, i) => {
+              return <div key={i} className="cell pulse" />
+            })}
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 PatternTable.propTypes = {
